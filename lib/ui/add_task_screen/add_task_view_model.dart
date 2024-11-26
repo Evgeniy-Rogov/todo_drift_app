44541b8b01f7abaa99c259d7edda7c8f996088c8
Evgeniy Rogov <zhentus@gmail.com>
2024-11-26 10:45:01 +0300

    Add view_model for add_task_screen

---
+++ lib/ui/add_task_screen/add_task_view_model.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/domain/repositories/tasks_repository.dart';
+
+class AddTaskViewModel with ChangeNotifier {
+  final _tasksRepositoty = TasksRepository();
+  final taskController = TextEditingController();
+  var _isAddProcess = false;
+
+  bool get isAddProcess => _isAddProcess;
+
+  void addTask(int caseId) async {
+    if (taskController.text.isEmpty) return;
+
+    _isAddProcess = true;
+    notifyListeners();
+    await _tasksRepositoty.addTask(taskController.text, caseId);
+    _isAddProcess = false;
+    notifyListeners();
+  }
+
+  @override
+  void dispose() {
+    taskController.dispose();
+    super.dispose();
+  }
+}
