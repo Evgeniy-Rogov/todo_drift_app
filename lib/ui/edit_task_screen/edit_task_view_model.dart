49d51cb5ee3740c4f53a4b239ea0ebb65870a80d
Evgeniy Rogov <zhentus@gmail.com>
2024-11-26 10:46:31 +0300

    Add view_model for edit_task_screen

---
+++ lib/ui/edit_task_screen/edit_task_view_model.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/domain/entities/edit_task_screen_data.dart';
+import 'package:todo_app/domain/repositories/tasks_repository.dart';
+
+class EditTaskViewModel with ChangeNotifier {
+  final _tasksRepository = TasksRepository();
+  late TextEditingController editTaskController;
+  var _isUpdateProcess = false;
+  final EditTaskScreenData data;
+
+  EditTaskViewModel(this.data) {
+    editTaskController = TextEditingController(text: data.body);
+  }
+
+  bool get isUpdateProcess => _isUpdateProcess;
+
+  void updateTask() async {
+    final text = editTaskController.text;
+    if (text.isEmpty) return;
+    if (text == data.body) return;
+
+    _isUpdateProcess = true;
+    notifyListeners();
+    await _tasksRepository.updateTask(data.id, text);
+    _isUpdateProcess = false;
+    notifyListeners();
+  }
+
+  @override
+  void dispose() {
+    editTaskController.dispose();
+    super.dispose();
+  }
+}
