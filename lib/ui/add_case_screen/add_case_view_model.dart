c2b79bedf19ee1ff6104771900cd442741d52e4b
Evgeniy Rogov <zhentus@gmail.com>
2024-11-26 10:32:18 +0300

    Add view_model for add_case_sqreen

---
+++ lib/ui/add_case_screen/add_case_view_model.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/domain/repositories/cases_repository.dart';
+
+class AddCaseViewModel with ChangeNotifier {
+  final _casesRepository = CasesRepository();
+  final caseController = TextEditingController();
+  var _isAddProcess = false;
+
+  bool get isAddProcess => _isAddProcess;
+
+  void addCase() async {
+    if (caseController.text.isEmpty) return;
+
+    _isAddProcess = true;
+    notifyListeners();
+    await _casesRepository.addCase(caseController.text);
+    _isAddProcess = false;
+    notifyListeners();
+  }
+
+  @override
+  void dispose() {
+    caseController.dispose();
+    super.dispose();
+  }
+}
