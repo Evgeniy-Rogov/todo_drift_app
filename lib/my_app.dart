a2725676bddc420a3075f63c1cf2328bd5bb3580
Evgeniy Rogov <zhentus@gmail.com>
2024-11-26 10:00:46 +0300

    Add MyApp

---
+++ lib/my_app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/navigation/main_navigator.dart';
+import 'package:todo_app/themes/main_theme.dart';
+
+class MyApp extends StatelessWidget {
+  static final mainNavigator = MainNavigator();
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'todo_drift_app',
+      theme: MainTheme.theme,
+      routes: mainNavigator.routes,
+      onGenerateRoute: mainNavigator.onGenerateRoute,
+    );
+  }
+}
